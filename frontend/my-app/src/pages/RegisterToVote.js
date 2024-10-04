5c502bf4d1dd0371243a5f85ff4e7136501def20
peachcreamsoda <maryamosyu@berkeley.edu>
2024-10-03 20:36:37 -0700

    adding Register to Vote page

---
+++ frontend/my-app/src/pages/RegisterToVote.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './cards.css'; 
+
+function RegisterToVote() {
+  return (
+    <div className="register-to-vote">
+      <h1>Register To Vote</h1>
+      <p>Here you can add the form or content for the registration process.</p>
+      {/* Add form or other content here */}
+    </div>
+  );
+}
+
+export default RegisterToVote;
